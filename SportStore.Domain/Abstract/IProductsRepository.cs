0829a071b48ab5dd93af0d5dccb7b42eb75408c9
Mohamed Ben Abbes <mohamedbenabbes6@gmail.com>
2020-11-14 10:44:23 +0100

    init

---
+++ SportStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,12 @@
+﻿using SportStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> Products { get; }
+    }
+}
---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        
+        public ProductController(IProductsRepository productsRepository)
+        {
+            this.repository = productsRepository;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);   
+        }
+    }
+}
